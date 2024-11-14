6ba610eb72f4e7aa0ea069505a66c254d345c39c
itc-s23024 <s23024@std.it-college.ac.jp>
2024-11-14 14:17:10 +0900

    ナビゲーション完成

--- app/src/main/java/jp/ac/it_college/std/s23024/pokequiz/ui/navigation/PokeQuizNavGraph.kt
+++ app/src/main/java/jp/ac/it_college/std/s23024/pokequiz/ui/navigation/PokeQuizNavGraph.kt
@@ -4,11 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import jp.ac.it_college.std.s23024.pokequiz.ui.generation.GenerationScreen
 import jp.ac.it_college.std.s23024.pokequiz.ui.home.HomeScreen
+import jp.ac.it_college.std.s23024.pokequiz.ui.navigation.PokeQuizDestinationsArgs.CORRECT_ANSWER_COUNT_ARG
+import jp.ac.it_college.std.s23024.pokequiz.ui.navigation.PokeQuizDestinationsArgs.GENERATION_ID_ARG
+import jp.ac.it_college.std.s23024.pokequiz.ui.quiz.QuizScreen
+import jp.ac.it_college.std.s23024.pokequiz.ui.result.ResultScreen
 
 @Composable
 fun PokeQuizNavGraph(
@@ -23,6 +29,7 @@ fun PokeQuizNavGraph(
         navController = navController,
         startDestination = PokeQuizDestinations.HOME_ROUTE
     ) {
+        //タイトル画面
         composable(
             route = PokeQuizDestinations.HOME_ROUTE
         ) {
@@ -33,10 +40,43 @@ fun PokeQuizNavGraph(
             )
         }
 
+        //世代選択画面
         composable(
             route = PokeQuizDestinations.GENERATION_ROUTE
         ) {
-            GenerationScreen()
+            GenerationScreen(
+                onGenerationSelected = {
+                    navActions.navigateToQuiz(it)
+                }
+            )
+        }
+
+        //クイズ画面
+        composable(
+            route = PokeQuizDestinations.QUIZ_ROUTE,
+            arguments = listOf(
+                navArgument(GENERATION_ID_ARG) {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            QuizScreen(
+                toResult = {
+                    navActions.navigateToResult(it)
+                }
+            )
+        }
+
+        //リザルト画面
+        composable(
+            route = PokeQuizDestinations.RESULT_ROUTE,
+            arguments = listOf(
+                navArgument(CORRECT_ANSWER_COUNT_ARG) {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            ResultScreen()
         }
     }
 }
